ca2e569cf59d8e84ae73fa6f918e5a5813675799
Nikolay <54248452+nickrockets@users.noreply.github.com>
2020-09-25 22:20:45 +0200

    Add files via upload
    Basic calculator. Calculates the sum of two numbers.

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "Enter a number. \n";
+    cin >> a;
+
+    cout << "Enter a new number. \n";
+    cin >> b;
+
+    sum = a + b;
+    cout << "The sum of those numbers is " << sum << endl;
+
+    return 0;
+}
